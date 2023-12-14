5cdbf47101bf2ac2ae91b277a302bc1e6e3be03a
Admin <purohitparthiv@gmail.com>
2023-12-15 00:26:26 +0530

    fix

--- src/main/java/com/driver/Vehicle.java
+++ src/main/java/com/driver/Vehicle.java
@@ -45,6 +45,27 @@ public class Vehicle {
         return this.name;
     }
 
+    public static class Boat implements WaterVehicle
+    {
+        public String vehicleName;
+
+        public int vehicleCapacity;
+
+        public Boat(String vehicleName,int vehicleCapacity)
+        {
+            this.vehicleName = vehicleName;
+            this.vehicleCapacity = vehicleCapacity;
+        }
+        public String getVehicleName()
+        {
+            return this.vehicleName;
+        }
+        public int getVehicleCapacity()
+        {
+            return this.vehicleCapacity;
+        }
+    }
+
 
 
 }
